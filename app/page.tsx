12dee6dedabb74e5f97da49f08a1de932a439bf5
Ricardo Yegros <ricardo_yegros@hotmail.com>
2023-04-08 04:37:46 -0300

    login page done and semi responsive, fix size of landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,20 @@
+'use client';
+import { useRouter } from 'next/navigation';
 import Image from "next/image";
+import { publicRoutes } from 'routes/routes.models';
 
 export default function Root() {
+  const router = useRouter();
   return (
-    <div className="grid place-content-center h-screen w-screen bg-slate-200">
-    <div className=" from-purple bg-gradient-to-tr  sm:h-[26rem] sm:w-[38rem] h-[26rem] w-[18rem] border-2 border-solid border-black grid place-content-center ">
-    <div className="flex sm:flex-row flex-col gap-4 bg-white justify-center items-center rounded-2xl p-6 font-bold h-[20rem] w-[16rem] sm:h-80 sm:w-[32rem]">
+    <div className="grid place-content-center h-screen w-screen bg-gradient-to-t from-slate-400">
+    <div className=" from-purple bg-gradient-to-tr  sm:h-[42rem] sm:w-[68rem] h-[26rem] w-[18rem]  grid place-content-center ">
+    <div className="flex sm:flex-row flex-col gap-4 bg-white justify-center items-center rounded-2xl p-6 font-bold h-[20rem] w-[16rem] sm:h-[32rem] sm:w-[52rem]">
       <div className="flex flex-col gap-4 justify-center items-center">
-        <h1 className="text-3xl text-grey ">
+        <h1 className="text-3xl text-grey">
           BIBLIO<span className="text-purple">METRICA</span>
         </h1>
         <p className="text-gray-500 text-lg font-semibold">Decide con Datos</p>
-        <button className="hover:font-bold font-semibold bg-purple text-white rounded-lg h-9 w-32 text-center">Comenzar</button>
+        <button onClick={()=>router.push(publicRoutes.Login)} className="hover:font-bold hover:underline font-semibold bg-purple text-white rounded-lg h-9 w-32 text-center">Comenzar</button>
       </div>
       <Image src="https://img.freepik.com/vector-gratis/libro-lectura-joven-estudiante-estudiando-interior-biblioteca-mujer-sentada-escritorio-estantes-estanteria-llena-libros-ilustracion-vectorial-plana-educacion-conocimiento-concepto-club-lectores_74855-21149.jpg?w=2000" alt="logo landing" width={230} height={230} />
     </div>
