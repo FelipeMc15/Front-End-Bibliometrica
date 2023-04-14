fead87b1c75b31fd2802a755f524f6952ad78a6f
miqueasledesma <mickeas98@gmail.com>
2023-04-14 19:46:00 -0300

    update:layout/home

---
+++ app/home/components/HomeCard.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import React from "react";
+import testImage from "public/assets/testImage.jpg";
+function HomeCard({}) {
+  return (
+    <div className="w-64 h-80 flex flex-col gap-2 justify-center items-center overflow-hidden bg-white shadow-sm shadow-slate-600 p-2 hover:shadow-lg transition-all duration-500">
+      <Image src={testImage} alt="imagen_perrito.jpg" className="w-full h-44" />
+      <p className="text-slate-500 font-normal text-sm text-center">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis,
+        consectetur.
+      </p>
+      <button className="py-2 w-fit mx-2 text-sm px-4 rounded-md text-slate-100 shadow-sm shadow-slate-300 bg-indigo-700 hover:scale-105 hover:bg-blue-700 transition-all duration-500">
+        Solicitar
+      </button>
+    </div>
+  );
+}
+
+export default HomeCard;
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,16 +1,22 @@
+import React from "react";
+import HomeCard from "./components/HomeCard";
+import HomeFooter from "./components/HomeFooter";
 export default function HomePage() {
+  function fakeInfo(n: number): JSX.Element[] {
+    const r = [];
+    for (let i = 0; i < n; i++) {
+      r.push(<HomeCard />);
+    }
+    return r;
+  }
+
   return (
-    <h1 className="text-[12rem]">
-      Hola mundo Hola mundo Hola mundo Hola mundo Hola mundo Hola mundo Hola
-      mundo Hola mundo Hola mundo Hola mundo Hola mundo Hola mundo Hola mundo
-      Hola mundo Hola mundo Hola mundo Hola mundo Hola mundo Hola mundo Hola
-      mundo Hola mundo Hola mundo Hola mundo Hola mundo Hola mundo Hola mundo
-      Hola mundo Hola mundo Hola mundo Hola mundo Hola mundo Hola mundo Hola
-      mundo Hola mundo Hola mundo Hola mundo Hola mundo Hola mundo Hola mundo
-      Hola mundo Hola mundo Hola mundo Hola mundo Hola mundo Hola mundo Hola
-      mundo Hola mundo Hola mundo Hola mundo Hola mundo Hola mundo Hola mundo
-      Hola mundo Hola mundo Hola mundo Hola mundo Hola mundo Hola mundo Hola
-      mundo Hola mundo{" "}
-    </h1>
+    <div className="w-full flex flex-col items-center gap-4 pt-2">
+      <div className="bg-slate-500 w-1/3 h-24"></div>
+      <section className="w-full flex flex-row gap-4 flex-wrap items-center justify-center">
+        {fakeInfo(6)}
+      </section>
+      <HomeFooter />
+    </div>
   );
 }
