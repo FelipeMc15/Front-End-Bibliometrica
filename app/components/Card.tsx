0587d09b2f07412c07dda3233b31323ac4a66e65
miqueasledesma <mickeas98@gmail.com>
2023-04-27 18:39:37 -0300

    feat:dinamic route(forms)

--- app/components/Card.tsx
+++ app/components/Card.tsx
@@ -1,7 +1,17 @@
+"use client";
 import Image from "next/image";
 import React from "react";
 import testImage from "../../public/assets/1200px-Biblioteca-montserrat.jpg";
+import { useRouter } from "next/navigation";
+
 function GeneralCard() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    const id = 5;
+    router.push(`/home/services/${id}`);
+  };
+
   return (
     <div className="w-64 h-80 flex flex-col gap-2 justify-center items-center overflow-hidden bg-white hover:shadow-xl shadow-slate-600 p-2 transition-all duration-500">
       <Image src={testImage} alt="imagen_perrito.jpg" className="w-full h-44" />
@@ -9,7 +19,10 @@ function GeneralCard() {
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis,
         consectetur.
       </p>
-      <button className="py-2 w-fit mx-2 text-sm px-4 rounded-md text-slate-100 shadow-sm shadow-slate-300 bg-indigo-700 hover:scale-105 hover:bg-blue-700 transition-all duration-500">
+      <button
+        onClick={handleClick}
+        className="py-2 w-fit mx-2 text-sm px-4 rounded-md text-slate-100 shadow-sm shadow-slate-300 bg-indigo-700 hover:scale-105 hover:bg-blue-700 transition-all duration-500"
+      >
         Solicitar
       </button>
     </div>
